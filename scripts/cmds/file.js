edddbecff401bfef5caa252f9251c38fb1706dd3
mickey0617 <160757077+mickey0617@users.noreply.github.com>
2024-03-02 18:32:19 +0100

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,9 +15,9 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100042484217511",];
+    const permission = ["61553392844761",];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("- Paku, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
+      return api.sendMessage("- ✨ 𝐌𝐈𝐂𝐊𝐄𝐘 ✨, You don't have permission to use this command. 🐤", event.threadID, event.messageID);
     }
 
     const fileName = args[0];
